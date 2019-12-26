b1354e2da56d6d07267772777fc8f1a41e87424b
Adam Xiao <adam.x.xiao@gmail.com>
2019-12-26 00:01:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
   counter = 0
   sq_array = []
 
-  while array[counter] do
-    sq_array.push(array[counter] ** 2)
-    counter += 1
-  end
-  puts sq_array
+    while array[counter] do
+      sq_array.push(array[counter] ** 2)
+      counter += 1
+    end
+  sq_array
 end
